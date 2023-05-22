4ebcd611909d9e864b6fbc604d6de144eef0d413
Jacopo Gasparetto <jacopo.gasparetto@gmail.com>
2023-05-22 10:35:54 +0200

    Check velocity != instead of abs > 0

--- src/player.cpp
+++ src/player.cpp
@@ -40,7 +40,7 @@ void Player::_physics_process(float delta) {
   } else if (input->is_action_pressed("move_right")) {
     velocity.x = m_speed;
   }
-  abs(velocity.x) > 0 ? m_animatedSprite2D->play() : m_animatedSprite2D->stop();
+  velocity.x != 0 ? m_animatedSprite2D->play() : m_animatedSprite2D->stop();
 
   auto is_flipped = m_animatedSprite2D->is_flipped_h() && velocity.x == 0;
   m_animatedSprite2D->set_flip_h(velocity.x < 0 || is_flipped);
