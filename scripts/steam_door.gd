6fb456fd79b70ba4d034c08923a15b64529f5791
Jacopo Gasparetto <jacopo.gasparetto@gmail.com>
2023-07-13 23:12:55 +0200

    Add Door with lever scene

--- scripts/steam_door.gd
+++ scripts/steam_door.gd
@@ -1,6 +1,42 @@
-extends GPUParticles2D
+extends Node2D
 
+@export var closed = true
+var collision_layer = 0
 
-func _on_area_2d_body_entered(body):
+func _ready():
+	$Lever/Label.hide()
+	collision_layer = $Door/StaticBody2D.collision_layer
+
+func show_label(_body):
+	$Lever/Label.show()
+
+func hide_label(_body):
+	$Lever/Label.hide()
+
+func interact():
+	if closed:
+		close()
+	else:
+		open()
+
+func open():
+	$Lever/AnimationPlayer.play("Close")
+	$Door/Area2D.monitoring = true
+	$Door/SteamParticles.emitting = true
+	$Door/StaticBody2D.set_physics_process(true)
+	$Door/StaticBody2D.collision_layer = collision_layer
+	$Door.show()
+	closed = true
+
+func close():
+	$Lever/AnimationPlayer.play("Open")
+	$Door/Area2D.monitoring = false
+	$Door/SteamParticles.emitting = false
+	$Door/StaticBody2D.set_physics_process(false)
+	$Door/StaticBody2D.collision_layer = 0
+	$Door.hide()
+	closed = false
+
+func on_body_entered(body):
 	if (body.is_in_group("Player")):
-		body.call("hit");
+		body.call("hit"); # Replace with function body.
