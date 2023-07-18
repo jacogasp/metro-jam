f4d46e09dfb36ffe483d8db5da4ef435a3cb00d3
Jacopo Gasparetto <jacopo.gasparetto@gmail.com>
2023-07-18 12:38:34 +0200

    Migrate to Godot v4.1

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -59,10 +59,10 @@ void uninitialize_example_module(ModuleInitializationLevel p_level) {
 extern "C" {
 // Initialization.
 GDExtensionBool GDE_EXPORT
-example_library_init(const GDExtensionInterface* p_interface,
-                     const GDExtensionClassLibraryPtr p_library,
+example_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address,
+                     GDExtensionClassLibraryPtr p_library,
                      GDExtensionInitialization* r_initialization) {
-  godot::GDExtensionBinding::InitObject init_obj(p_interface, p_library,
+  godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library,
                                                  r_initialization);
 
   init_obj.register_initializer(initialize_example_module);
