afe9063f75e2ff2dac53852c56686fe0de27c3fe
Jacopo Gasparetto <jacopo.gasparetto@gmail.com>
2023-07-18 20:12:45 +0200

    Silence more warnings in GDCLASS_V2

--- src/macros.hpp
+++ src/macros.hpp
@@ -5,19 +5,27 @@
 #ifdef __clang__
 #  define DISABLE_ALL_WARNINGS_BEGIN                                           \
     _Pragma("clang diagnostic push")                                           \
-    _Pragma("clang diagnostic ignored \"-Wunused-parameter\"")
+    _Pragma("clang diagnostic ignored \"-Wunused-parameter\"")                 \
+    _Pragma("clang diagnostic ignored \"-Wall\"")                              \
+    _Pragma("clang diagnostic ignored \"-Wextra\"")                            \
+    _Pragma("clang diagnostic ignored \"-Wpedantic\"")                         \
+    _Pragma("clang diagnostic ignored \"-Wsign-conversion\"")
 #  define DISABLE_ALL_WARNINGS_END _Pragma("clang diagnostic pop")
 #elif defined __GNUC__
 #  define DISABLE_ALL_WARNINGS_BEGIN                                           \
     _Pragma("GCC diagnostic push")                                             \
-    _Pragma("GCC diagnostic ignored \"-Wunused-parameter\"")
+    _Pragma("GCC diagnostic ignored \"-Wunused-parameter\"")                   \
+    _Pragma("GCC diagnostic ignored \"-Wall\"")                                \
+    _Pragma("GCC diagnostic ignored \"-Wextra\"")                              \
+    _Pragma("GCC diagnostic ignored \"-Wpedantic\"")                           \
+    _Pragma("GCC diagnostic ignored \"-Wsign-conversion\"")                                                                               \
 # define DISABLE_ALL_WARNINGS_END _Pragma("GCC diagnostic pop")
 #endif
+//clang-format on
 
 #define GDCLASS_V2(Object, Type)                                               \
   DISABLE_ALL_WARNINGS_BEGIN                                                   \
   GDCLASS(Object, Type)                                                        \
   DISABLE_ALL_WARNINGS_END
 
-//clang-format on
 #endif // COREGAME_MACROS_HPP
