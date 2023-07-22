7f13288baeb6af77b5ab275fb579a2cfce237e46
Jacopo Gasparetto <jacopo.gasparetto@gmail.com>
2023-07-22 14:39:18 +0200

    Add DyingState and DieCommand

--- src/enemy_soldier.cpp
+++ src/enemy_soldier.cpp
@@ -1,10 +1,85 @@
 #include "enemy_soldier.hpp"
+#include "health_bar.hpp"
+#include <godot_cpp/classes/animated_sprite2d.hpp>
+#include <godot_cpp/classes/engine.hpp>
+
+EnemySoldier::IdleState EnemySoldier::idle   = EnemySoldier::IdleState();
+EnemySoldier::DyingState EnemySoldier::dying = EnemySoldier::DyingState();
+static auto constexpr die                    = EnemySoldier::DieCommand();
 
 void EnemySoldier::_bind_methods() {
+  BIND_PROPERTY(EnemySoldier, gravity, godot::Variant::FLOAT);
+  ClassDB::bind_method(D_METHOD("take_hit"), &EnemySoldier::take_hit);
+}
+
+void EnemySoldier::_ready() {
+  m_health        = m_total_health;
+  auto health_bar = get_node<HealthBar>("HealthBar");
+  if (health_bar) {
+    health_bar->set_max(m_health);
+    health_bar->set_value(m_health);
+  }
 }
 
 void EnemySoldier::_process(double) {
+  m_state->update(*this);
 }
 
-void EnemySoldier::take_hit(int) {
+void EnemySoldier::take_hit(int damage) {
+  m_health -= damage;
+  auto health_bar = get_node<HealthBar>("HealthBar");
+  if (health_bar) {
+    health_bar->set_value(m_health);
+  }
+  if (m_health <= 0) {
+    die(*this);
+  }
+}
+
+void EnemySoldier::_physics_process(double delta) {
+  if (Engine::get_singleton()->is_editor_hint()) {
+    return;
+  }
+
+  auto velocity = get_velocity();
+  velocity.y    = m_gravity * static_cast<float>(delta);
+  set_velocity(velocity);
+  move_and_slide();
+}
+
+void EnemySoldier::set_gravity(float gravity) {
+  m_gravity = gravity;
+}
+
+float EnemySoldier::get_gravity() const {
+  return m_gravity;
+}
+
+void EnemySoldier::set_total_health(int health) {
+  m_total_health = health;
+}
+
+void EnemySoldier::set_state(EnemySoldierState* state) {
+  m_state = state;
+}
+
+// Commands
+void EnemySoldier::DieCommand::operator()(EnemySoldier& enemy) const {
+  auto animated_sprite = enemy.get_node<AnimatedSprite2D>("AnimatedSprite2D");
+  if (animated_sprite) {
+    animated_sprite->play("Die");
+  }
+  enemy.set_state(&EnemySoldier::dying);
+}
+
+// States
+void EnemySoldier::IdleState::update(EnemySoldier&) const {
+}
+
+void EnemySoldier::DyingState::update(EnemySoldier& enemy) const {
+  auto animated_sprite = enemy.get_node<AnimatedSprite2D>("AnimatedSprite2D");
+  if (animated_sprite && animated_sprite->is_playing()) {
+    return;
+  }
+  enemy.queue_free();
 }
--- src/enemy_soldier.hpp
+++ src/enemy_soldier.hpp
@@ -5,20 +5,54 @@
 
 #include "damageable.hpp"
 #include "macros.hpp"
+#include "state.hpp"
 
 using namespace godot;
+
+class EnemySoldier;
+using EnemySoldierState = State<EnemySoldier>;
 
 class EnemySoldier
     : public CharacterBody2D
     , public Damageable {
-
   GDCLASS_V2(EnemySoldier, CharacterBody2D)
+
  private:
+  float m_gravity            = 980;
+  int m_total_health         = 100;
+  int m_health               = 100;
+  EnemySoldierState* m_state = &EnemySoldier::idle;
+
   static void _bind_methods();
 
  public:
+  void _ready() override;
   void _process(double) override;
-  void take_hit(int) override;
+  void _physics_process(double) override;
+  void set_state(EnemySoldierState* state);
+  void take_hit(int damage) override;
+  void set_gravity(float gravity);
+  [[nodiscard]] float get_gravity() const;
+  void set_total_health(int health);
+  [[nodiscard]] int get_total_health() const;
+
+  // States
+  struct IdleState : EnemySoldierState {
+    void update(EnemySoldier& enemy) const override;
+  };
+
+  struct DyingState : EnemySoldierState {
+    void update(EnemySoldier& enemy) const override;
+  };
+
+ public:
+  static IdleState idle;
+  static DyingState dying;
+
+  // Commands
+  struct DieCommand {
+    void operator()(EnemySoldier& enemy) const;
+  };
 };
 
 #endif // COREGAME_ENEMY_SOLDIER_HPP
