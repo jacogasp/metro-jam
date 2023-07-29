b34d668d0020566ac023666eb421c3758b3b7a36
Jacopo Gasparetto <jacopo.gasparetto@gmail.com>
2023-07-29 22:24:11 +0200

    Poison ranger is pushed back on hits

--- src/enemy_soldier.cpp
+++ src/enemy_soldier.cpp
@@ -4,14 +4,18 @@
 #include <godot_cpp/classes/animated_sprite2d.hpp>
 #include <godot_cpp/classes/engine.hpp>
 
-EnemySoldier::IdleState EnemySoldier::idle   = EnemySoldier::IdleState();
-EnemySoldier::DyingState EnemySoldier::dying = EnemySoldier::DyingState();
-static auto constexpr die                    = EnemySoldier::DieCommand();
+EnemySoldier::IdleState EnemySoldier::idle       = EnemySoldier::IdleState();
+EnemySoldier::FallingState EnemySoldier::falling = EnemySoldier::FallingState();
+EnemySoldier::DyingState EnemySoldier::dying     = EnemySoldier::DyingState();
+static auto constexpr go_idle                    = EnemySoldier::IdleCommand();
+static auto constexpr die                        = EnemySoldier::DieCommand();
+static auto constexpr hit                        = EnemySoldier::HitCommand();
 
 void EnemySoldier::_bind_methods() {
   BIND_PROPERTY(EnemySoldier, gravity, Variant::FLOAT);
   BIND_PROPERTY(EnemySoldier, total_health, Variant::INT);
   BIND_PROPERTY(EnemySoldier, fire_rate, Variant::FLOAT);
+  BIND_PROPERTY(EnemySoldier, hit_bounce_velocity, Variant::VECTOR2);
   ClassDB::bind_method(D_METHOD("take_hit"), &EnemySoldier::take_hit);
 }
 
@@ -35,19 +39,7 @@ void EnemySoldier::_ready() {
 void EnemySoldier::_process(double delta) {
   if (Engine::get_singleton()->is_editor_hint())
     return;
-  m_state->update(*this);
   m_fire_timer.tick(delta);
-}
-
-void EnemySoldier::take_hit(int damage) {
-  m_health -= damage;
-  auto health_bar = get_node<HealthBar>("HealthBar");
-  if (health_bar) {
-    health_bar->set_value(m_health);
-  }
-  if (m_health <= 0) {
-    die(*this);
-  }
 }
 
 void EnemySoldier::_physics_process(double delta) {
@@ -55,10 +47,23 @@ void EnemySoldier::_physics_process(double delta) {
     return;
   }
 
+  m_state->update(*this);
   auto velocity = get_velocity();
-  velocity.y    = m_gravity * static_cast<float>(delta);
+  velocity.y += m_gravity * static_cast<float>(delta);
   set_velocity(velocity);
   move_and_slide();
+}
+
+void EnemySoldier::take_hit(int damage, Vector2 const& from_direction) {
+  m_health -= damage;
+  hit(*this, from_direction);
+  auto health_bar = get_node<HealthBar>("HealthBar");
+  if (health_bar) {
+    health_bar->set_value(m_health);
+  }
+  if (m_health <= 0) {
+    die(*this);
+  }
 }
 
 void EnemySoldier::set_state(EnemySoldierState* state) {
@@ -88,11 +93,24 @@ void EnemySoldier::set_fire_rate(double rate) {
 double EnemySoldier::get_fire_rate() const {
   return m_fire_rate;
 }
+
 void EnemySoldier::fire() {
   auto gun = get_node<Gun>("Gun");
   if (gun) {
     gun->fire({});
   }
+}
+
+void EnemySoldier::set_hit_bounce_velocity(const Vector2& velocity) {
+  m_hit_bounce_velocity = velocity;
+}
+
+Vector2 EnemySoldier::get_hit_bounce_velocity() const {
+  return m_hit_bounce_velocity;
+}
+
+EnemySoldier::Direction EnemySoldier::get_direction() const {
+  return m_direction;
 }
 
 // Commands
@@ -104,8 +122,33 @@ void EnemySoldier::DieCommand::operator()(EnemySoldier& enemy) const {
   enemy.set_state(&EnemySoldier::dying);
 }
 
+void EnemySoldier::HitCommand::operator()(EnemySoldier& enemy,
+                                          Vector2 const& from_direction) const {
+  auto const position  = enemy.get_global_position();
+  auto const direction = position.x > from_direction.x ? 1.0f : -1.0f;
+  auto bounce_velocity = enemy.get_hit_bounce_velocity();
+  bounce_velocity.x *= direction;
+  auto velocity = enemy.get_velocity();
+  velocity += bounce_velocity;
+  enemy.set_velocity(velocity);
+  enemy.set_state(&EnemySoldier::falling);
+}
+
 // States
 void EnemySoldier::IdleState::update(EnemySoldier&) const {
+}
+
+void EnemySoldier::FallingState::update(EnemySoldier& enemy) const {
+  auto const v_y = enemy.get_velocity().y;
+  if (v_y >= 0 && enemy.is_on_floor()) {
+    go_idle(enemy);
+  } else {
+    auto const animated_sprite =
+        enemy.get_node<AnimatedSprite2D>("AnimatedSprite2D");
+    if (animated_sprite) {
+      animated_sprite->play("Fall");
+    }
+  }
 }
 
 void EnemySoldier::DyingState::update(EnemySoldier& enemy) const {
@@ -114,4 +157,13 @@ void EnemySoldier::DyingState::update(EnemySoldier& enemy) const {
     return;
   }
   enemy.queue_free();
+}
+
+void EnemySoldier::IdleCommand::operator()(EnemySoldier& enemy) const {
+  auto animated_sprite = enemy.get_node<AnimatedSprite2D>("AnimatedSprite2D");
+  if (animated_sprite) {
+    animated_sprite->play("Idle");
+  }
+  enemy.set_velocity({0, 0});
+  enemy.set_state(&EnemySoldier::idle);
 }
--- src/enemy_soldier.hpp
+++ src/enemy_soldier.hpp
@@ -18,12 +18,16 @@ class EnemySoldier
     , public Damageable {
   GDCLASS_V2(EnemySoldier, CharacterBody2D)
 
+  enum Direction { left = -1, right = 1 };
+
  private:
-  float m_gravity            = 980;
-  int m_total_health         = 100;
-  int m_health               = 100;
-  EnemySoldierState* m_state = &EnemySoldier::idle;
-  TimeDelta m_fire_rate      = 1;
+  float m_gravity               = 980;
+  int m_total_health            = 100;
+  int m_health                  = 100;
+  EnemySoldierState* m_state    = &EnemySoldier::idle;
+  TimeDelta m_fire_rate         = 1;
+  Vector2 m_hit_bounce_velocity = {100, -100};
+  Direction m_direction         = right;
   Timer m_fire_timer;
 
   static void _bind_methods();
@@ -33,17 +37,25 @@ class EnemySoldier
   void _process(double) override;
   void _physics_process(double) override;
   void set_state(EnemySoldierState* state);
-  void take_hit(int damage) override;
+  void take_hit(int damage, Vector2 const& from_direction) override;
   void set_gravity(float gravity);
   [[nodiscard]] float get_gravity() const;
   void set_total_health(int health);
   [[nodiscard]] int get_total_health() const;
   void set_fire_rate(double rate);
   [[nodiscard]] double get_fire_rate() const;
+  void set_hit_bounce_velocity(Vector2 const& velocity);
+  [[nodiscard]] Vector2 get_hit_bounce_velocity() const;
+  [[nodiscard]] Direction get_direction() const;
+
   void fire();
 
   // States
   struct IdleState : EnemySoldierState {
+    void update(EnemySoldier& enemy) const override;
+  };
+
+  struct FallingState : EnemySoldierState {
     void update(EnemySoldier& enemy) const override;
   };
 
@@ -54,10 +66,19 @@ class EnemySoldier
  public:
   static IdleState idle;
   static DyingState dying;
+  static FallingState falling;
 
   // Commands
+  struct IdleCommand {
+    void operator()(EnemySoldier& enemy) const;
+  };
+
   struct DieCommand {
     void operator()(EnemySoldier& enemy) const;
+  };
+
+  struct HitCommand {
+    void operator()(EnemySoldier& enemy, Vector2 const& from_direction) const;
   };
 };
 
