497403f849c51acdbc43b87eea425999c65f61fb
Jacopo Gasparetto <jacopo.gasparetto@gmail.com>
2023-08-08 20:22:30 +0200

    Remove unused classes

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -5,9 +5,6 @@
 #include "register_types.hpp"
 
 #include "bumblebee.hpp"
-#include "chest.hpp"
-#include "coin.hpp"
-#include "coin_spawner.hpp"
 #include "constants.hpp"
 #include "gate.hpp"
 #include "grenade.hpp"
@@ -31,9 +28,6 @@ void initialize_example_module(ModuleInitializationLevel p_level) {
   core_game::crate_savings_directory(core_game::SAVINGS_DIRECTORY);
   ClassDB::register_class<MainScene>();
   ClassDB::register_class<Player>();
-  ClassDB::register_class<Coin>();
-  ClassDB::register_class<CoinSpawner>();
-  ClassDB::register_class<Chest>();
   ClassDB::register_class<World>();
   ClassDB::register_class<Gate>();
   ClassDB::register_class<LifeBar>();
