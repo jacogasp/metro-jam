9f91fed13343a48e24a1c7fe8fa1e40fc1ad9443
Jacopo Gasparetto <jacopo.gasparetto@gmail.com>
2023-08-10 23:06:16 +0200

    Persist PlatformSystem state to disk

--- scripts/PlatformSystem.gd
+++ scripts/PlatformSystem.gd
@@ -3,12 +3,15 @@ extends Node2D
 
 @export var enabled = false
 @export var one_shot = false
+var path = "user://savings/"
 
 var shutdown = false
 
 func _ready():
 	update_platforms_state()
 	$Handle/Label.hide()
+	path += get_path().get_concatenated_names().replace("/", "-")
+	load_state()
 
 func enable():
 	enabled = true
@@ -41,7 +44,37 @@ func interact():
 		disable()
 	else:
 		enable()
+	save_state()
 
 func on_body_entered(body):
 	if (body.is_in_group("Player")):
 		body.call("hit"); # Replace with function body.
+
+func save_state():
+	var state = {
+		"enabled": enabled,
+		"one_shot": one_shot,
+		"shutdown": shutdown
+	}
+	var json = JSON.stringify(state)
+	var file = FileAccess.open(path, FileAccess.WRITE)
+	file.store_line(json)
+	print("PlatformSystem state saved to ", path)
+
+func load_state():
+	if not FileAccess.file_exists(path):
+		print("PlatformSystem save file not found")
+		return false
+	
+	var file = FileAccess.open(path, FileAccess.READ)
+	var buffer = file.get_as_text()
+	var json = JSON.parse_string(buffer)
+	enabled = json["enabled"]
+	one_shot = json["one_shot"]
+	shutdown = json["shutdown"]
+	if enabled:
+		enable()
+	else:
+		disable()
+	print("PlatformSystem state loaded from ", path)
+	return true
