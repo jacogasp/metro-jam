b537a7bd3569c3b38eee5e1cd2e308cd0481869d
Jacopo Gasparetto <jacopo.gasparetto@gmail.com>
2023-08-12 11:13:19 +0200

    Fix Vector2 include header

--- src/weapon.hpp
+++ src/weapon.hpp
@@ -1,14 +1,14 @@
 #ifndef WEAPON_HPP
 #define WEAPON_HPP
 
-#include <godot_cpp/templates/vector.hpp>
+#include <godot_cpp/variant/vector2.hpp>
 
 using namespace godot;
 
 class Weapon {
  public:
-  virtual ~Weapon()                        = default;
-  virtual void fire(Vector2 const& direction) = 0;
+  virtual ~Weapon()                                  = default;
+  virtual void fire(godot::Vector2 const& direction) = 0;
 };
 
 #endif // WEAPON_HPP
