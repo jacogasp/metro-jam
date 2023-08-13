fbe017b56230bb35dd0e177fae1600fe6cabee2e
Jacopo Gasparetto <jacopo.gasparetto@gmail.com>
2023-08-13 17:48:36 +0200

    Add ExitKey collectible logic

--- scripts/ExitKey.gd
+++ scripts/ExitKey.gd
@@ -1,11 +1,8 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func body_entered(body):
+	if body.is_in_group("Player"):
+		$Area2D.queue_free()
+		$Label.queue_free()
+		body.call_deferred("pick", self)
